b0010ad1dad7a1a2ad7bb562f3227dad69347d4d
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2023-12-21 06:22:42 +0700

    Update

--- T3 - Regression/S00 - Utils/utils.py
+++ T3 - Regression/S00 - Utils/utils.py
@@ -1,7 +1,9 @@
-import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 from sklearn.metrics import r2_score
 from sklearn.metrics import mean_squared_error
+from sklearn.linear_model import LinearRegression, Ridge, Lasso, ElasticNet
+import json
 
 
 def _regress(ax, X, y, y_pred, title, color="steelblue"):
@@ -12,15 +14,25 @@ def _regress(ax, X, y, y_pred, title, color="steelblue"):
 
 
 def plot_reg(X_train, X_test, y_train, y_test, y_train_pred, y_test_pred):
+    # Indices for sorting during plotting
+    idx_train = np.argsort(X_train, axis=0).ravel()
+    idx_test = np.argsort(X_test, axis=0).ravel()
+
     fig, ax = plt.subplots(
         1, 2, figsize=(10, 5), constrained_layout=True, sharex=True, sharey=True
     )
-    _regress(ax=ax[0], X=X_train, y=y_train, y_pred=y_train_pred, title="Train")
+    _regress(
+        ax=ax[0],
+        X=X_train[idx_train, :].ravel(),
+        y=y_train[idx_train],
+        y_pred=y_train_pred[idx_train],
+        title="Train",
+    )
     _regress(
         ax=ax[1],
-        X=X_test,
-        y=y_test,
-        y_pred=y_test_pred,
+        X=X_test[idx_test, :].ravel(),
+        y=y_test[idx_test],
+        y_pred=y_test_pred[idx_test],
         title="Test",
         color="limegreen",
     )
@@ -48,8 +60,20 @@ def plot_res(y_train, y_test, y_train_pred, y_test_pred):
     fig.supylabel("Residual")
 
 
+def isLinearModel(model):
+    if (
+        isinstance(model, LinearRegression)
+        or isinstance(model, Ridge)
+        or isinstance(model, Lasso)
+        or isinstance(model, ElasticNet)
+    ):
+        return True
+    else:
+        return False
+
+
 def store_results(
-    results, model_name, model, y_train, y_test, y_train_pred, y_test_pred
+    results, model_name, model, y_train, y_test, y_train_pred, y_test_pred, params={}
 ):
     MSE_train = mean_squared_error(y_train, y_train_pred)
     MSE_test = mean_squared_error(y_test, y_test_pred)
@@ -57,14 +81,22 @@ def store_results(
     R2_train = r2_score(y_train, y_train_pred)
     R2_test = r2_score(y_test, y_test_pred)
 
+    coef = None
+    intercept = None
+
+    if isLinearModel(model):
+        coef = np.linalg.norm(model.coef_)
+        intercept = model.intercept_
+
     data = {
         "Model": model_name,
-        "Coef": model.coef_[0],
-        "Intercept": model.intercept_,
+        "Coef": coef,
+        "Intercept": intercept,
         "MSE Train": MSE_train,
         "MSE Test": MSE_test,
         "R2 Train": R2_train,
         "R2 Test": R2_test,
+        "Params": json.dumps(params),
     }
     results.append(data)
     return
